a484205afd1614fcfcfd038ae3c04e1d8d787b85
Johan Jordaan <johanjordaan@nightwish.local>
2016-12-07 07:51:28 +1100

    Added the dropdown's

--- DDPAssignment/ui.R
+++ DDPAssignment/ui.R
@@ -1,26 +1,32 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(leaflet)
 
-
-
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Is this mushroom poisonous?"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
+       #spore-print-color:black=k,brown=n,buff=b,chocolate=h,green=r,orange=o,purple=u,white=w,yellow=y
+       selectInput("spore-print-color","Spore Print Color",c("Black","Brown","Chocolate","Green","Orange","Purple","White","Yellow")),
+
+       #gill-color       : black=k,brown=n,buff=b,chocolate=h,gray=g, green=r,orange=o,pink=p,purple=u,red=e,white=w,yellow=y
+       selectInput("gill-color","Gill Color",c("Black","Brown","Buff","Chocolate","Gray","Green","Orange","Pink","Purple","Red","White","Yellow")),
+       
+       #gill-size: broad=b,narrow=n
+       selectInput("gill-size","Gill Size",c("Broad","Narrow")),
+       
+       #stalk-root: bulbous=b,club=c,cup=u,equal=e,rhizomorphs=z,rooted=r,missing=?
+       selectInput("stalk-root","Stalk Root",c("Bulbous","Club","Cup","Equal","Rhizomorphs","Rooted","Missing")),
+       
+       #population: abundant=a,clustered=c,numerous=n,scattered=s,several=v,solitary=y
+       selectInput("population","Populationt",c("Abundant","Clustered","Numerous","Scattered","Several","Solitary")),
+      
+       #habitat: grasses=g,leaves=l,meadows=m,paths=p,urban=u,waste=w,woods=d
+       selectInput("habitat","Habitat",c("Grasses","Leaves","Meadows","Paths","Urban","Waste","Woods")),
+       
+       
+             
        sliderInput("bins",
                    "Number of bins:",
                    min = 1,
@@ -28,10 +34,12 @@ shinyUI(fluidPage(
                    value = 30)
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot"),
-       leafletOutput("map")
+      tabsetPanel(
+        tabPanel("Prediction", plotOutput("distPlot")),
+        tabPanel("Model Paremeters", leafletOutput("map")),
+        tabPanel("Help", tableOutput("Help"))
+      )
     )
   )
 ))
